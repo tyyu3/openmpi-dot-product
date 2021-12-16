b4ddf13896bfc6fa6617192cb0216765b180427c
tyyu3 <tyyu@list.ru>
2021-12-16 16:09:05 +0300

    Pragma omp

--- main.cpp
+++ main.cpp
@@ -6,9 +6,6 @@
 #include <random>
 #include <vector>
 
-
-static constexpr std::size_t iterations = 1;
-
 template <typename T>
 inline __attribute__((always_inline)) void do_not_optimize(T& value)
 {
@@ -39,18 +36,6 @@ struct Timings
     double ns_per_iter;
 };
 
-std::vector<int> hardcoded_vec(std::uint64_t n, std::uniform_int_distribution<int>& uid, std::mt19937& rng, std::uint64_t myid, size_t numprocs)
-{
-    std::vector<int> res;
-    res.resize(n);
-    for(std::size_t i = 0; i < n; i += 1)
-    {
-        res[i] = uid(rng);
-    }
-    return res;
-
-}
-
 std::vector<int> random_vec(std::uint64_t n, std::uniform_int_distribution<int>& uid, std::mt19937& rng, std::uint64_t myid, size_t numprocs)
 {
     std::vector<int> res;
@@ -63,28 +48,13 @@ std::vector<int> random_vec(std::uint64_t n, std::uniform_int_distribution<int>&
 
 }
 
-std::int64_t dot_product(const std::vector<int>& v1, const std::vector<int>&v2, std::uint64_t myid, size_t numprocs)
-{
-    std::int64_t res = 0, finalres = 0;
-    for(std::int64_t i = myid; i < v1.size(); i +=numprocs)
-    {
-
-        //std::cout << res <<"+="<<v1[i]<<"*"<<v2[i] << std::endl;
-        res += v1[i]*v2[i];
-    }
-    //std::cout << "local res " << res << std::endl;
-    MPI_Barrier(MPI_COMM_WORLD);
-    MPI_Reduce(&res, &finalres, 1, MPI_INT64_T, MPI_SUM, 0, MPI_COMM_WORLD);
-    return finalres;
-}
-
-
 int main(int argc, char **argv)
 {
   int myid, numprocs;
-  std::uint64_t n = 320; //vector len
+  int n = 16*1000*1000ll*100; //vector len
 
-  static std::mt19937 rng{1};
+  std::random_device rd;
+  static std::mt19937 rng(rd());
   std::uniform_int_distribution<int> uid(-5,5);
 
   MPI_Init(&argc, &argv);
@@ -92,44 +62,38 @@ int main(int argc, char **argv)
   MPI_Comm_rank(MPI_COMM_WORLD, &myid);
   char processor_name[MPI_MAX_PROCESSOR_NAME];
   int name_len;
+
+  auto time_start = std::chrono::high_resolution_clock::now();
+  auto time_end = time_start;
+
+  auto time_now = std::chrono::system_clock::to_time_t(time_start);
   MPI_Get_processor_name(processor_name, &name_len);
-  std::cout << "I'm #" << myid << " of " << numprocs <<" on " << processor_name << '\n';
+  std::cout << "I'm #" << myid << " of " << numprocs <<" on " << processor_name << " at " <<  std::ctime(&time_now) << '\n';
 
   std::vector<int> v1, v2, local_v1, local_v2;
-  std::uint64_t local_n = 0;
+  int local_n = 0;
   int local_dot = 0, dot = 0;
-  if(myid == 0)
-  {
-    v1 = random_vec(n, uid, rng, myid, numprocs);
-    v2 = v1;
-    local_n = v1.size() / numprocs;
-  }
 
-  /*generation start
+  //generation start
+  v1.resize(n);
+  v2.resize(n);
   if(myid==0)
   {
-      v1.resize(n);
-      v2.resize(n);
-      local_n = v1.size() / numprocs;
+      local_n = v1.size() / (unsigned)numprocs;
   }
-  MPI_Bcast(&local_n, 1, MPI_UINT64_T, 0, MPI_COMM_WORLD);
+  MPI_Bcast(&local_n, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
   local_v1.resize(local_n);
   local_v2.resize(local_n);
-  for (std::uint64_t i = 0; i < local_n; i++)
+  for (int i = 0; i < local_n; i++)
   {
    local_v1[i]=uid(rng);
    local_v2[i]=uid(rng);
   }
   MPI_Gather(local_v1.data(), local_n, MPI_INT, v1.data(), local_n, MPI_INT, 0, MPI_COMM_WORLD);
   MPI_Gather(local_v2.data(), local_n, MPI_INT, v2.data(), local_n, MPI_INT, 0, MPI_COMM_WORLD);
-  *///generation end
+  //generation end
 
-  /*if(myid==0)
-    for(auto i : v1)
-        std::cout << i <<std::endl;*/
-
-  auto time_start = std::chrono::high_resolution_clock::now();
-  auto time_end = std::chrono::high_resolution_clock::now();
   unsigned long ticks_start, ticks_end;
   if(myid==0)
   {
@@ -138,15 +102,18 @@ int main(int argc, char **argv)
       ticks_start = ticks();
       local_n = v1.size() / numprocs;
   }
-  MPI_Bcast(&local_n, 1, MPI_UINT64_T, 0, MPI_COMM_WORLD);
+  MPI_Bcast(&local_n, 1, MPI_INT, 0, MPI_COMM_WORLD);
   local_v1.resize(local_n);
   local_v2.resize(local_n);
   MPI_Scatter(v1.data(), local_n, MPI_INT, local_v1.data(), local_n, MPI_INT, 0, MPI_COMM_WORLD);
   MPI_Scatter(v2.data(), local_n, MPI_INT, local_v2.data(), local_n, MPI_INT, 0, MPI_COMM_WORLD);
-  for (std::uint64_t i = 0; i < local_n; i++)
+
+  #pragma omp parallel
+  for (int i = 0; i < local_n; i++)
   {
    local_v1[i] *= local_v2[i];
   }
+  #pragma omp parallel
   for (auto i : local_v1)
   {
       local_dot += i;
@@ -169,21 +136,6 @@ int main(int argc, char **argv)
       std::cout << "dot is " << dot << '\n';
   }
 
-
-/*
-  //MPI_Bcast(&s, 1, MPI_INT, 0, MPI_COMM_WORLD);
-  std::int64_t dot;
-  {
-
-      //--------------------
-      //for (std::size_t i = 0; i < iterations; ++i)
-      //{
-          dot = dot_product(v1, v2, myid, numprocs);
-          do_not_optimize(dot);
-      //}
-      //--------------------
-
-  }*/
   MPI_Finalize();
   return 0;
 }
